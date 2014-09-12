04c8c5744925a8ef16402dca94d64680dc4840d9
Tony Wang <ivytony@gmail.com>
2014-09-12 10:34:06 -0600

    updated trim.sh

--- trim.sh
+++ trim.sh
@@ -1,4 +1,4 @@
 while read line; do 
 	echo $line 
-	trim_galore --paired --gz --fastqc -o CUHN076_repeated_Samples_20140212/fastq_trim4 $line;
+	trim_galore --fastqc -o CUHN076_repeated_Samples_20140212/fastq_trim4 $line --paired
 done < 076OldReadsToTrim.txt
